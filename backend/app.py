6c3c788b8bcbb9303ac7e7560b3e67483cb7522f
elmasibrahim <elmas.ibrahim05@yahoo.com>
2024-12-19 20:00:49 +0100

    Zwischenstand

---
+++ backend/app.py
@@ -0,0 +1,71 @@
+"""
+API File
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/Schulter")
+def schulter() -> dict:
+    """
+    Function for getting shoulder exercises
+    """
+    return {
+        "Shoulder Press": "Hanteln nach oben pressen mit Bank aufrecht",
+        "Rowing": "Rückwärts am Butterfly",
+    }
+
+
+@app.route("/Rücken")
+def ruecken() -> dict:
+    """
+    Function for getting back exercises
+    """
+    return {
+        "Latzug": "Zwei Griffe nach unten ziehen",
+        "Cable Row": "Sitzen und Dreieck nach hinten ziehen",
+    }
+
+
+@app.route("/Brust")
+def brust() -> dict:
+    """
+    Function for getting chest exercises
+    """
+    return {
+        "Brustpresse": "Am Gerät aufrecht Griffe nach vorne drücken",
+        "Butterfly": "Am Gerät, isoliert",
+    }
+
+
+@app.route("/Bizeps")
+def bizeps() -> dict:
+    """
+    Function for getting bizeps exercises
+    """
+    return {"Hammer Curls": "Hanteln vertikal", "Preacher Curls": "Hanteln horizontal"}
+
+
+@app.route("/Trizeps")
+def trizeps() -> dict:
+    """
+    Function for getting trizeps exercises
+    """
+    return {
+        "Trizeps Pushdowns": "Kabelozug nach unten ziehen",
+        "Trizepsdrücken": "An der Maschine Griffe nach vorne",
+    }
+
+
+@app.route("/Beine")
+def beine() -> dict:
+    """
+    Function for getting leg exercises
+    """
+    # Hier bei den Beinübungen weitermachen
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
