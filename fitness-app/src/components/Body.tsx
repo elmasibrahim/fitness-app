f6cfd3b40305d449f88200c79c605f2af0d94db5
elmasibrahim <elmas.ibrahim05@yahoo.com>
2024-12-21 09:46:15 +0100

    #11 Arrow function zum Abfragen der Daten aus dem Backend

--- fitness-app/src/components/Body.tsx
+++ fitness-app/src/components/Body.tsx
@@ -1,8 +1,17 @@
 import Sidebar from "./Sidebar";
 import Content from "./Content";
 import "./styles/Body.css";
+import { useState } from "react";
 
 const Body = () => {
+  const [data, setData] = useState(null);
+
+  const fetchData = async (path: string) => {
+    const response = await fetch("http://127.0.0.1:5000/" + path);
+    const result = await response.json();
+    setData(result);
+  };
+
   return (
     <div className="row">
       <div className="col-2 left-col">
