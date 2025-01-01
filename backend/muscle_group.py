120b9e5ef6ed46e5fd4918f9257d1480d4ea3f9c
elmasibrahim <elmas.ibrahim05@yahoo.com>
2025-01-01 14:27:53 +0100

    #20 Klassenmethode, um eine Muskelgruppe anhand des Namens zu bekommen

--- backend/muscle_group.py
+++ backend/muscle_group.py
@@ -2,11 +2,26 @@
 Module containing the MuscleGroup ORM class
 """
 
+from typing import Type
+from db import DB
+
 
 class MuscleGroup:
     """
     ORM class
     """
+
+    @classmethod
+    def get_muscle_group_by_name(cls, name: str) -> Type["MuscleGroup"]:
+        """
+        Class method for getting a MuscleGroup instance by the name of the muscle group
+        """
+        query = "SELECT mid FROM muscleGroup WHERE name = %s"
+        params = (name,)
+        data = DB.fetch(query, params)
+        mid = data[0][0]
+        instance = cls(mid, name)
+        return instance
 
     def __init__(self, mid: int, name: str):
         self.__mid = mid
