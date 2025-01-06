0d22f36f36e31abd42ca6460f82b5163a357c2bd
elmasibrahim <elmas.ibrahim05@yahoo.com>
2025-01-06 10:58:33 +0100

    #22 Card-Komponente angepasst

--- fitness-app/src/components/Card.tsx
+++ fitness-app/src/components/Card.tsx
@@ -3,14 +3,16 @@ import "./styles/Card.css";
 interface Props {
   name: string;
   description: string;
+  weight: number;
 }
 
-const Card = ({ name, description }: Props) => {
+const Card = ({ name, description, weight }: Props) => {
   return (
     <div className="card">
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
         <p className="card-text">{description}</p>
+        <p className="card-text">Weight: {weight} kg</p>
       </div>
     </div>
   );
