4608a3c7b7eb95a627d4c162c6a62b63b1155943
elmasibrahim <elmas.ibrahim05@yahoo.com>
2025-01-11 10:05:14 +0100

    #24 API-Schnittstelle für Details einer Exercise implementiert

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ API File
 
 from flask import Flask
 from flask_cors import CORS
+from exercise import Exercise
 from muscle_group import MuscleGroup
 
 app = Flask(__name__)
@@ -13,11 +14,25 @@ CORS(app)
 @app.route("/<muscle_group>")
 def exercises(muscle_group: str) -> list:
     """
-    Flask method for getting the exercises of a muscle group
+    Flask function for getting the exercises of a muscle group
     """
     ins = MuscleGroup.get_muscle_group_by_name(muscle_group)
     ins.add_exercises()
     data = [exercise.eid for exercise in ins.exercises]
+    return data
+
+
+@app.route("/exercise/<int:eid>")
+def exercise_details(eid: int) -> dict:
+    """
+    Flask function for getting detailled information about a exercise
+    """
+    exercise = Exercise.get_exercise_by_eid(eid)
+    data = {
+        "name": exercise.name,
+        "description": exercise.description,
+        "weight": exercise.weight,
+    }
     return data
 
 
