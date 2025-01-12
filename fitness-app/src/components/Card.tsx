928631b8b9fa1b923671a893b93130c2a6118403
elmasibrahim <elmas.ibrahim05@yahoo.com>
2025-01-12 16:09:29 +0100

    #24 Abfrage von Exercise-Daten in der Card-Komponente

--- fitness-app/src/components/Card.tsx
+++ fitness-app/src/components/Card.tsx
@@ -1,16 +1,29 @@
+import { useEffect, useState } from "react";
 import "./styles/Card.css";
+import get from "./helpers/request";
 
 interface Props {
   eid: number;
 }
 
 const Card = ({ eid }: Props) => {
+  const [data, setData] = useState({ description: "", name: "", weight: 0.0 });
+
+  useEffect(() => {
+    const getData = async () => {
+      const path = "exercise/" + eid;
+      const data = await get(path);
+      setData(data);
+    };
+    getData();
+  }, []);
+
   return (
     <div className="card">
       <div className="card-body">
-        <h5 className="card-title">{eid}</h5>
-        <p className="card-text">Text</p>
-        <p className="card-text">Weight</p>
+        <h5 className="card-title">{data.name}</h5>
+        <p className="card-text">{data.description}</p>
+        <p className="card-text">Weight: {data.weight} kg</p>
       </div>
     </div>
   );
