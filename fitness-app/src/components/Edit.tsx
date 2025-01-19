8c367f8111cbd07a30779db7533a29409b9855eb
elmasibrahim <elmas.ibrahim05@yahoo.com>
2025-01-19 19:01:26 +0100

    #28 Erhoehung und Senkung von Gewicht beim Bearbeiten implementiert

--- fitness-app/src/components/Edit.tsx
+++ fitness-app/src/components/Edit.tsx
@@ -1,24 +1,27 @@
 import Button from "./Button";
+import { useState } from "react";
 
 interface Props {
   weight: number;
 }
 
 const Edit = ({ weight }: Props) => {
+  const [editWeight, setEditWeight] = useState(weight);
+
+  const handleMinusClick = () => {
+    setEditWeight(editWeight - 0.5);
+  };
+
+  const handlePlusClick = () => {
+    setEditWeight(editWeight + 0.5);
+  };
+
   return (
     <>
       <div className="d-flex justify-content-center align-items-center mt-1">
-        <Button
-          color="secondary"
-          icon="dash"
-          onClick={() => console.log("Clicked minus")}
-        />
-        <p className="card-text mx-4">{weight} kg</p>
-        <Button
-          color="secondary"
-          icon="plus"
-          onClick={() => console.log("Clicked plus")}
-        />
+        <Button color="secondary" icon="dash" onClick={handleMinusClick} />
+        <p className="card-text mx-4">{editWeight} kg</p>
+        <Button color="secondary" icon="plus" onClick={handlePlusClick} />
       </div>
       <div className="d-flex justify-content-center align-items-center mt-2">
         <div className="mx-2">
