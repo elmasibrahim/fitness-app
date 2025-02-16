a1bb3f4d015dec6a58452fe67c8f516f7b958138
elmasibrahim <elmas.ibrahim05@yahoo.com>
2025-02-16 15:56:42 +0100

    #31 Wechsel zu Add-Modus implementiert

--- fitness-app/src/components/Content.tsx
+++ fitness-app/src/components/Content.tsx
@@ -7,23 +7,28 @@ interface Props {
 }
 
 const Content = ({ data }: Props) => {
-
   const [add, setAdd] = useState(false);
 
   const handleAddClick = () => {
     setAdd(true);
-  }
+  };
 
   return (
     <div className="container text-center mt-4">
-      <Button color="primary" icon="plus" onClick={handleAddClick} />
-      <div className="row row-cols-3">
-        {data.map((eid) => (
-          <div className="col mt-5" key={eid}>
-            <Card eid={eid} />
+      {add ? (
+        <h1>Übung hinzufügen</h1>
+      ) : (
+        <>
+          <Button color="primary" icon="plus" onClick={handleAddClick} />
+          <div className="row row-cols-3">
+            {data.map((eid) => (
+              <div className="col mt-5" key={eid}>
+                <Card eid={eid} />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      )}
     </div>
   );
 };
